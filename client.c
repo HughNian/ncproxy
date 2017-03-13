2f8611a02646d1d49bcae11680b699eafc9a7908
HughNian <Hugh.Nian@163.com>
2017-03-13 22:42:20 +0800

    :trollface:

--- client.c
+++ client.c
@@ -1,7 +1,17 @@
 #include "client.h"
 
 int
-client_init(void)
+client_init(client *c)
 {
+    c = (client *)_zalloc(sizeof(*c));
 
+    if(NULL == c){
+        fprintf(stderr, "client init failed, error msg(%s)\n", strerror(errno));
+        return -1;
+    }
+
+    c->len = 0;
+    c->size = 0;
+    c->used = 0;
+    c->cost_time = time(NULL);
 }
